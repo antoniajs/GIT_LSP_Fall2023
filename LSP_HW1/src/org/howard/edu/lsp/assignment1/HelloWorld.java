614d5a5925cbb13cbff0c33b64ef3091fca76119
ajs02 <antonia.jaimeson-sh@bison.howard.edu>
2023-08-31 15:56:29 -0400

    Initial commit/

---
+++ LSP_HW1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Name: Antonia Jaimeson-Shirley
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); 
+	}
+
+}
