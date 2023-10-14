c798e9876ffdf31f85b316b251c55ba209379172
ajs02 <antonia.jaimeson-sh@bison.howard.edu>
2023-10-14 15:29:41 -0400

    added a comment

--- LSP_HW1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP_HW1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -7,7 +7,7 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World"); 
+		System.out.println("Hello World"); //printing to the system
 	}
 
 }
