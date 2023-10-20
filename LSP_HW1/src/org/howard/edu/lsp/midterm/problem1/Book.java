423f247ae4660a131963e9cc3269cf9b2aa161f1
ajs02 <antonia.jaimeson-sh@bison.howard.edu>
2023-10-19 20:19:20 -0400

    updated problem 1

--- LSP_HW1/src/org/howard/edu/lsp/midterm/problem1/Book.java
+++ LSP_HW1/src/org/howard/edu/lsp/midterm/problem1/Book.java
@@ -50,16 +50,4 @@ public class Book {
 		
 }
 
-public class Driver{
-	public static void main (String[] args) {
-		Book book1 = new Book("To Kill a MockingBird", "Harper Lee", 1960);
-		Book book2 = new Book("To Kill a MockingBird", "Harper Lee", 1960);
-		Book book3 = new Book("1984", "George Orwell", 1960);
-		
-		System.out.println("Book1 equals Book2: " + book1.equals(book2));
-		System.out.println("Book1 equals Book3: " + book1.equals(book3));
-		System.out.println("Display of book's title, author, and year: " + book1.toString());
-		
-	}
-	
-}
+
