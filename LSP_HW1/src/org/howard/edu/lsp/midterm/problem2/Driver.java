02b084b9f107805bbd994d99067196676537ce11
ajs02 <antonia.jaimeson-sh@bison.howard.edu>
2023-10-19 20:25:28 -0400

    problem 2

---
+++ LSP_HW1/src/org/howard/edu/lsp/midterm/problem2/Driver.java
@@ -0,0 +1,13 @@
+package org.howard.edu.lsp.midterm.problem2;
+
+public class Driver {
+    public static void main(String[] args) {
+        Range range1 = new Range(5, 10);
+        Range range2 = new Range(8, 15);
+        int testValue = 7;
+
+        System.out.println("Is " + testValue + " in range? " + range1.isValuesInRange(testValue));
+        System.out.println("Do Range1 and Range2 overlap? " + range1.doRangesOverlap(range2));
+        System.out.println("Size of Range1: " + range1.size());
+    }
+}
---
+++ LSP_HW1/src/org/howard/edu/lsp/midterm/problem2/Range.java
@@ -0,0 +1,24 @@
+package org.howard.edu.lsp.midterm.problem2;
+
+public class Range {
+    private int start;
+    private int end;
+
+    public Range(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public boolean isValuesInRange(int value) {
+        return value >= start && value <= end;
+    }
+
+    public boolean doRangesOverlap(Range Range2) {
+        return this.start <= Range2.end && Range2.start <= this.end;
+    }
+
+    public int size() {
+        return Math.abs(end - start) + 1;
+    }
+}
+
